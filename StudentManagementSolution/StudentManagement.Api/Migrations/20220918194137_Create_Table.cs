f2489c81d0fa4866d0b04eb26b2477ac6a0c0f65
Mugesh Shanmugavel <mugesh.shanmugavel@justanswer.com>
2022-09-19 01:13:56 +0530

    Added Readme.Md file and DB Scripts

---
+++ StudentManagementSolution/StudentManagement.Api/Migrations/20220918194137_Create_Table.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagement.Api.Migrations
+{
+    public partial class Create_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
